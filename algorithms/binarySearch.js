de5d213f2802d5492623e56f908b7647186a7e9c
kartik <kartikth40@gmail.com>
2022-08-19 12:44:03 +0530

    added binary search

---
+++ algorithms/binarySearch.js
@@ -0,0 +1,32 @@
+// BINARY SEARCH
+
+// Time Complexity
+// best    - O(1) 
+// worst   - O(log n)
+// average - O(log n)
+
+// Space Complexity
+//         - O(1)
+
+const binarySearch = (array, x) => {
+	let size = array.length
+	let low = 0
+	let high = array[size-1]
+
+	while(low <= high) {
+		let mid = low + parseInt((high - low) / 2)
+
+		if(array[mid] === x) return mid
+		else if(array[mid] < x) low = mid + 1
+		else high = mid - 1
+	}
+	return -1
+}
+
+
+let arr = [3,7,5,8,1,4,0,9]
+let x = 1
+let searchIndex = binarySearch(arr, x)
+console.log("array -> "+arr)
+console.log("search -> "+x)
+console.log("Index -> "+searchIndex)
