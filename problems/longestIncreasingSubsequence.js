d8144907b51e9ed7c3424617e12dab5781241383
kartikth40 <kartikth40@gmail.com>
2023-06-05 16:06:26 +0530

    corrected solution for ques 300

--- problems/longestIncreasingSubsequence.js
+++ problems/longestIncreasingSubsequence.js
@@ -1,30 +1,28 @@
-// LONGEST INCREASING SUBSEQUENCE
-// For a sequence a1, a2,..., an, find the length of
-// the longest increasing subsequence ai1, ai2,...,aik
-// such that i1 < i2 <...<ik and ai1 < ai2 <...< aik
+// 300. LONGEST INCREASING SUBSEQUENCE
+// Given an integer array nums, return the
+// length of the longest strictly increasing subsequence.
+// https://leetcode.com/problems/longest-increasing-subsequence
 
 // for example =>
 // [5  2  8  6  3  6  9  5]
 //     |________|__|__|
 
-const longestIncreasingSubsequence = (arr) => {
-  let dp = new Array(arr.length).fill(1)
-  for (let i = 1; i < arr.length; i++) {
-    let currentLongestLength = 0
+const longestIncreasingSubsequence = (nums) => {
+  let dp = new Array(nums.length).fill(1)
+  let longest = 1
+  for (let i = 1; i < nums.length; i++) {
     for (let j = 0; j < i; j++) {
-      if (arr[j] < arr[i] && currentLongestLength < arr[j]) {
-        currentLongestLength = dp[j]
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+        longest = Math.max(longest, dp[i])
       }
     }
-
-    dp[i] = 1 + currentLongestLength
   }
-
-  return Math.max(...dp)
+  return longest
 }
 
-const arr = [5, 2, 8, 6, 3, 6, 9, 5]
-let len = longestIncreasingSubsequence(arr)
+const nums = [5, 2, 8, 6, 3, 6, 9, 5]
+let len = longestIncreasingSubsequence(nums)
 
 console.log(len)
 
